45a5ccb3907a05b469b6cac5ada68c336695dd2d
Khangle081020 <khangle08102004@gmail.com>
2026-01-14 08:35:36 +0700

    com108-cs21301-lab2-Khangle0810204

--- bai5.c
+++ bai5.c
@@ -7,13 +7,41 @@
 // BÀI LÀM THÊM
 // BÀI 5: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TÍCH VÀ THƯƠNG CỦA 2 SỐ
 
+/******************************************************************************
+ * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
+ * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
+ * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ *****************************************************************************/
+
+// BÀI LÀM THÊM
+// BÀI 5: XÂY DỰNG CHƯƠNG TRÌNH TÍNH TÍCH VÀ THƯƠNG CỦA 2 SỐ
+
 #include <stdio.h>
 
 int main(){
     int so1, so2;
+    int tich;
+    float thuong;
 
-    printf("Chương trình tính tích và thương của 2 số\n");
+    printf("Chuong trinh tinh tich va thuong cua 2 so\n");
 
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &so1);
+
+    printf("Nhap so thu hai: ");
+    scanf("%d", &so2);
+
+    // Tính tích
+    tich = so1 * so2;
+    printf("Tich cua %d va %d la: %d\n", so1, so2, tich);
+
+    // Tính thương
+    if (so2 != 0) {
+        thuong = (float)so1 / so2;
+        printf("Thuong cua %d va %d la: %.2f\n", so1, so2, thuong);
+    } else {
+        printf("Khong the tinh thuong vi so thu hai bang 0\n");
+    }
 
     return 0;
-} 
+}
